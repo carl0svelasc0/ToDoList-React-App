768bacada523c36c10c43170aba26617e1fb63e6
Carlos Velasco <carlosandresvelasco@outlook.com>
2022-11-09 19:23:00 -0500

    Added styles

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,11 +1,16 @@
 import React from "react";
+import './ToDoItem.css'
 
-function ToDoItem (props){
+function ToDoItem(props) {
     return (
-        <li>
-            <span>C</span>
-            <p>{props.text}</p>
-            <span>X</span>
+        <li className="ToDoItem">
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+                √
+            </span>
+            <p className={`ToDoItem-p ${props.completed && 'ToDoItem-p--complete'}`}>
+                {props.text}
+                </p>
+            <span className="Icon Icon-delete">X</span>
         </li>
     );
 }
